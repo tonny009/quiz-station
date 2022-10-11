b5f4c9e15b165f323a329631e8f2a1a937a72043
tonny009 <tabassum.islam009@gmail.com>
2022-10-11 23:17:39 +0900

    Title pic done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,36 +1,33 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Topic from '../Topics/Topic';
+import pic from '../../images/quizTime.png'
+import picture from '../../images/quix.jpeg'
+import './Home.css';
 
 const Home = () => {
     const topics = useLoaderData();
     const { data } = topics;
     return (
 
-        <div className='container'>
-            {/* <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-                <div className='carousel-inner'>
-                    <div className='carousel-item active'>
-                        <img src="..." class="d-block w-100" alt="...">
-                    </div>
-                    <div className='carousel-item'>
-                        <img src="..." className='d-block w-100' alt="...">
-                    </div>
-                    <div className='carousel-item'>
-                        <img src="..." className='d-block w-100' alt="...">
+        <div className='container '>
+            <div id="carouselExampleSlidesOnly" className='carousel slide title-pic' data-bs-ride="carousel">
+                <div className='carousel-inner rounded'>
+                    <div className='carousel-item active slide_pic'>
+                        <img src={pic} className='d-block w-100' alt="..." />
+                        <div className='carousel-caption  d-sm-block'>
+                            <p className='fs-1 fw-bolder text'>Test Yourself</p>
+                            <p className='fs-4'>This platform is for all self-lerner to push up their confidence with quiz assesments.Be one step forward to your career.</p>
+                        </div>
                     </div>
                 </div>
-            </div> */}
-            {/* <div className="img-container">
-                <h2>Here will be home image</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit doloribus facilis, tempora debitis amet optio minima beatae iure quas deserunt veniam asperiores pariatur magnam vitae, reiciendis non nobis temporibus dolor.</p>
-            </div> */}
-            <div className='row justify-content-center'>
+            </div>
+            < div className='row justify-content-center' >
                 {
                     data.map(topic => <Topic key={data.id} topic={topic}></Topic>)
                 }
-            </div>
-        </div>
+            </div >
+        </div >
     );
 };
 
