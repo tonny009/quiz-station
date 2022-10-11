14c6bc1c4f3c2673094964a18191eb93a5073d32
tonny009 <tabassum.islam009@gmail.com>
2022-10-11 21:21:12 +0900

    Statistics graph done

--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import './Statistics.css'
+// import React, { PureComponent } from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
+    const courseData = useLoaderData();
+    const { data } = courseData;
+    console.log(data);
     return (
-        <div>
-            <h2>This is Statistics</h2>
+        <div className='stat container'>
+
+            <BarChart width={430} height={450} data={data}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="total" fill="#8884d8" />
+
+            </BarChart>
+
+
         </div>
     );
 };
